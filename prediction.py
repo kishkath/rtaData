f96359a6fef95bf4d5890e3b59bac1583c5f0f85
kishkath <kiranchw000@gmail.com>
2022-12-01 21:56:31 +0530

    Files Initialization

--- prediction.py
+++ prediction.py
@@ -2,6 +2,16 @@ import joblib
 import numpy as np
 from sklearn.preprocessing import LabelEncoder
 from sklearn.ensemble import RandomForestClassifier
+import pandas as pd
+
+data = pd.read_csv("Dataset\cleanedData.csv")
+
+def encoding(input_val,colName):
+    le = LabelEncoder()
+    values = data[colName].unique()
+    data[colName] = le.fit_transform(data[colName])
+    dicts = dict(zip(values, data[colName].unique()))
+    return dicts[input_val]
 
 def get_predictions(data,model):
     '''
